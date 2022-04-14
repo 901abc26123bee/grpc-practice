1b9cd33f7929d6034ccabe55080fcf64866002ad
FA00265\tina.wong <tina.wong@shoalter.com>
2022-04-14 16:14:09 +0800

    client test completed

--- grpc-client/src/main/java/com/example/grpc/grpcclient/GrpcClientApplication.java
+++ grpc-client/src/main/java/com/example/grpc/grpcclient/GrpcClientApplication.java
@@ -1,11 +1,13 @@
 package com.example.grpc.grpcclient;
 
+import com.example.grpc.grpcclient.service.impl.GrpcHelloClientService;
+import javax.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GrpcClientApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(GrpcClientApplication.class, args);
 	}
